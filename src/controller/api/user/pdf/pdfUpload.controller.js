501ea389196130be545f467b07813f7a580fb74f
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-11-13 15:12:23 +0530

    new

--- src/controller/api/user/pdf/pdfUpload.controller.js
+++ src/controller/api/user/pdf/pdfUpload.controller.js
@@ -1,1 +1,15 @@
 const {PdfUrls,UserPdfSign} =  require("../../../../models")
+
+
+
+exports.upload = async(req,res)=>{
+    try{
+
+    }catch(erorr){
+        return res.status(400).json({
+            status:false,
+            status_code:400,
+            message:erorr
+        })
+    }
+}
--- src/route/pdfUpload/pdfUploadRoute.js
+++ src/route/pdfUpload/pdfUploadRoute.js
@@ -2,5 +2,6 @@ const express = require("express")
 const pdfUploadRote = express.Router()
 const pdfUploadController = require("../../controller/api/user/pdf/pdfUpload.controller")
 
+pdfUploadRote.post("/upload",pdfUploadController.upload)
 
 module.exports = pdfUploadRote;
