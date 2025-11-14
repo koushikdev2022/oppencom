a3f286a46b67502c12d1ca1902a9a0a7d4226059
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-11-14 11:20:29 +0530

    new

--- src/migrations/20251113091533-create_user_pds_signs_table.js
+++ src/migrations/20251113091533-create_user_pds_signs_table.js
@@ -1,63 +1,56 @@
 'use strict';
-const { Model } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-    class UserPdfSign extends Model {
-        static associate(models) {
-            // Define associations here if needed
-            // Example: this.belongsTo(models.PdfUrls, { foreignKey: 'pdf_table_id' });
-            // Example: this.belongsTo(models.User, { foreignKey: 'user_id' });
-        }
-    }
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user_pdf_signs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_pdf_url:{
+          type: Sequelize.STRING,
+           allowNull: false,
+      },
+      type:{
+          type: Sequelize.BIGINT,
+           allowNull: false,
+      },
+      pdf_table_id:{
+          type: Sequelize.BIGINT,
+           allowNull: false,
+      },
+      user_id:{
+          type: Sequelize.BIGINT,
+           allowNull: false,
+      },
+      status:{
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue:1
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+     });
+  },
 
-    UserPdfSign.init(
-        {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: DataTypes.INTEGER,
-            },
-            user_pdf_url: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            type: {
-                type: DataTypes.BIGINT,
-                allowNull: false,
-            },
-            pdf_table_id: {
-                type: DataTypes.BIGINT,
-                allowNull: false,
-            },
-            user_id: {
-                type: DataTypes.BIGINT,
-                allowNull: false,
-            },
-            status: {
-                type: DataTypes.INTEGER,
-                allowNull: true,
-                defaultValue: 1,
-            },
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-            updated_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-        },
-        {
-            sequelize,
-            modelName: 'UserPdfSign',
-            tableName: 'user_pdf_signs',
-            underscored: true,
-            timestamps: false,
-        }
-    );
-
-    return UserPdfSign;
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
 };
